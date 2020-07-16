880a7604db9bfd9766dad3bcf63b7019f5bad8c3
Oak Soe <oaksoekyaw@gmail.com>
2020-07-16 20:07:13 +0800

    added redis caching to implementing services

--- author.js
+++ author.js
@@ -1,76 +1,85 @@
 const { ApolloServer, gql } = require('apollo-server');
 const { buildFederatedSchema } = require('@apollo/federation');
+const { RedisCache } = require('apollo-server-cache-redis');
+const responseCachePlugin = require('apollo-server-plugin-response-cache');
 
 // Federation way
-const authorSchema = {
-    typeDefs: gql`
-    directive @cacheControl(
-        maxAge: Int,
-        scope: CacheControlScope
-    ) on OBJECT | FIELD_DEFINITION
-    
-    enum CacheControlScope {
-        PUBLIC
-        PRIVATE
-    }
-
-        type User @key(fields: "id") {
-            id: ID!
-            email: String @cacheControl(maxAge: 100)
-        }
-    
-        type Query {
-            userById(id: ID!): User @cacheControl(maxAge: 200)
-        }
-
-        extend type Chirp @key(fields: "id") {
-            id: ID! @external
-            authorId: ID! @external
-            author: User @requires(fields: "authorId")
-        }
-    `,
-    resolvers: {
-        Chirp: {
-            author: ({ authorId, ...args }) => {
-                return {id: 1, email: 'oakoak@gmail.com'};
-            },
-        },
-        Query: {
-            userById: (root, args, context, info) => {
-                // info.cacheControl.setCacheHint({ maxAge: 222 });
-                return {id: 1, email: 'oak@gmail.com'};
-            },
-        }
-    } 
-};
-
-const server = new ApolloServer({ 
-    schema: buildFederatedSchema(authorSchema)
-});
-
-// Non-federation way
 // const authorSchema = {
 //     typeDefs: gql`
-//         type User {
+//     directive @cacheControl(
+//         maxAge: Int,
+//         scope: CacheControlScope
+//     ) on OBJECT | FIELD_DEFINITION
+    
+//     enum CacheControlScope {
+//         PUBLIC
+//         PRIVATE
+//     }
+
+//         type User @key(fields: "id") {
 //             id: ID!
-//             email: String
+//             email: String @cacheControl(maxAge: 100)
 //         }
     
 //         type Query {
-//             userById(id: ID!): User
+//             userById(id: ID!): User @cacheControl(maxAge: 200)
+//         }
+
+//         extend type Chirp @key(fields: "id") {
+//             id: ID! @external
+//             authorId: ID! @external
+//             author: User @requires(fields: "authorId")
 //         }
 //     `,
 //     resolvers: {
+//         Chirp: {
+//             author: ({ authorId, ...args }) => {
+//                 return {id: 1, email: 'oakoak@gmail.com'};
+//             },
+//         },
 //         Query: {
 //             userById: (root, args, context, info) => {
-//                 info.cacheControl.setCacheHint({ maxAge: 999 });
+//                 // info.cacheControl.setCacheHint({ maxAge: 222 });
 //                 return {id: 1, email: 'oak@gmail.com'};
 //             },
 //         }
 //     } 
 // };
 
-// const server = new ApolloServer(authorSchema);
+// const server = new ApolloServer({ 
+//     schema: buildFederatedSchema(authorSchema)
+// });
+
+// Non-federation way
+const authorSchema = {
+    typeDefs: gql`
+        type User {
+            id: ID!
+            email: String
+        }
+    
+        type Query {
+            userById(id: ID!): User
+        }
+    `,
+    resolvers: {
+        Query: {
+            userById: (root, args, context, info) => {
+                info.cacheControl.setCacheHint({ maxAge: 999 });
+                return {id: 1, email: 'oak@gmail.com'};
+            },
+        }
+    } 
+};
+
+const server = new ApolloServer({
+    ...authorSchema,
+    cache: new RedisCache({
+        host: 'localhost', // 'redis-server',
+        // Options are passed through to the Redis client
+    }),
+    plugins: [responseCachePlugin()],
+});
 
 server.listen(4002).then(({ url }) => {
 console.log(`🚀 Server ready at ${url}`)
--- chirp.js
+++ chirp.js
@@ -1,88 +1,97 @@
 const { ApolloServer, gql } = require('apollo-server');
 const { buildFederatedSchema } = require('@apollo/federation');
+const { RedisCache } = require('apollo-server-cache-redis');
+const responseCachePlugin = require('apollo-server-plugin-response-cache');
 
 // Federation way
-const chirpSchema = {
-    typeDefs: gql`
-    directive @cacheControl(
-        maxAge: Int,
-        scope: CacheControlScope
-    ) on OBJECT | FIELD_DEFINITION
-    
-    enum CacheControlScope {
-        PUBLIC
-        PRIVATE
-    }
-
-        type Chirp @key(fields: "id") {
-            id: ID!
-            text: String
-            authorId: ID! @cacheControl(maxAge: 333)
-        }
-
-        type Query {
-            chirpById(id: ID!): Chirp @cacheControl(maxAge: 888)
-            chirpsByAuthorId(authorId: ID!): [Chirp] @cacheControl(maxAge: 777)
-        }
-
-        extend type User @key(fields: "id") {
-            id: ID! @external
-            chirps: [Chirp] @requires(fields: "id")
-        }
-    `,
-    resolvers: {
-        User: {
-            chirps: (id) => {
-                return [{id: 1, text: 'chirp by author oak', authorId: 1}];
-            },
-        },
-        Query: {
-            chirpById: (root, args, context, info) => {
-                // info.cacheControl.setCacheHint({ maxAge: 888 });
-                return {id: 1, text: 'first chirp', authorId: 1}
-            },
-            chirpsByAuthorId: (root, args, context, info) => {
-                // info.cacheControl.setCacheHint({ maxAge: 777 });
-                return [{id: 1, text: 'first chirp', authorId: 1}]
-            },
-        }
-    } 
-};
-
-const server = new ApolloServer({ 
-    schema: buildFederatedSchema([chirpSchema])
-});
-
-// Non-federation way
 // const chirpSchema = {
 //     typeDefs: gql`
-//         type Chirp {
+//     directive @cacheControl(
+//         maxAge: Int,
+//         scope: CacheControlScope
+//     ) on OBJECT | FIELD_DEFINITION
+    
+//     enum CacheControlScope {
+//         PUBLIC
+//         PRIVATE
+//     }
+
+//         type Chirp @key(fields: "id") {
 //             id: ID!
 //             text: String
-//             authorId: ID!
+//             authorId: ID! @cacheControl(maxAge: 333)
 //         }
 
 //         type Query {
-//             chirpById(id: ID!): Chirp
-//             chirpsByAuthorId(authorId: ID!): [Chirp]
+//             chirpById(id: ID!): Chirp @cacheControl(maxAge: 888)
+//             chirpsByAuthorId(authorId: ID!): [Chirp] @cacheControl(maxAge: 777)
+//         }
+
+//         extend type User @key(fields: "id") {
+//             id: ID! @external
+//             chirps: [Chirp] @requires(fields: "id")
 //         }
 //     `,
 //     resolvers: {
+//         User: {
+//             chirps: (id) => {
+//                 return [{id: 1, text: 'chirp by author oak', authorId: 1}];
+//             },
+//         },
 //         Query: {
 //             chirpById: (root, args, context, info) => {
-//                 info.cacheControl.setCacheHint({ maxAge: 888 });
+//                 // info.cacheControl.setCacheHint({ maxAge: 888 });
 //                 return {id: 1, text: 'first chirp', authorId: 1}
 //             },
 //             chirpsByAuthorId: (root, args, context, info) => {
-//                 info.cacheControl.setCacheHint({ maxAge: 777 });
+//                 // info.cacheControl.setCacheHint({ maxAge: 777 });
 //                 return [{id: 1, text: 'first chirp', authorId: 1}]
 //             },
 //         }
 //     } 
 // };
 
-// const server = new ApolloServer(chirpSchema);
+// const server = new ApolloServer({ 
+//     schema: buildFederatedSchema([chirpSchema])
+// });
+
+// Non-federation way
+const chirpSchema = {
+    typeDefs: gql`
+        type Chirp {
+            id: ID!
+            text: String
+            authorId: ID!
+        }
+
+        type Query {
+            chirpById(id: ID!): Chirp
+            chirpsByAuthorId(authorId: ID!): [Chirp]
+        }
+    `,
+    resolvers: {
+        Query: {
+            chirpById: (root, args, context, info) => {
+                info.cacheControl.setCacheHint({ maxAge: 888 });
+                return {id: 1, text: 'first chirp', authorId: 1}
+            },
+            chirpsByAuthorId: (root, args, context, info) => {
+                info.cacheControl.setCacheHint({ maxAge: 777 });
+                return [{id: 1, text: 'first chirp', authorId: 1}]
+            },
+        }
+    } 
+};
+
+const server = new ApolloServer({
+    ...chirpSchema,
+    cache: new RedisCache({
+        host: 'localhost', // 'redis-server',
+        // Options are passed through to the Redis client
+    }),
+    plugins: [responseCachePlugin()],
+});
 
 server.listen(4001).then(({ url }) => {
-console.log(`🚀 Server ready at ${url}`)
+    console.log(`🚀 Server ready at ${url}`)
 });
